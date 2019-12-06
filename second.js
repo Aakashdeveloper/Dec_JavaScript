40e47b0b1f926d73e21f829e3b3a059731ae2cb5
aakashDeveloper <ahanda205@gmail.com>
2019-12-06 01:15:04 +0000

    a

---
+++ second.js
@@ -0,0 +1,54 @@
+= assignment
+== comparing the value
+=== comparing data and datatype
+
+var a  = 10
+var b  == 10
+var c === "10"
+
+var a = 10
+undefined
+a
+10
+a == 10
+true
+a == 220
+false
+
+
+var a = 10
+undefined
+a
+10
+a == 10
+true
+a == 220
+false
+var a == "10"
+VM340:1 Uncaught SyntaxError: Unexpected token '=='
+a == "10"
+true
+a === "10"
+false
+
+// Es5
+var a = 10
+undefined
+a
+10
+var a = 20
+undefined
+a
+20
+
+var a = 10
+
+var a = "declaring"
+a = 10 "assigning a value"
+
+//Es6
+var = we can declare and reassign
+let = we cannot redeclare but can reassign 
+const = we cannot reassign nor redeclare
+
+
---
+++ third.js
@@ -0,0 +1,73 @@
+console.log() == "debug the code"
+alert
+confirm
+prompt
+
+console.log("hii")
+hii
+use in debgging  the application
+
+alert("From Submiited")
+Act like prompt for just message
+
+
+confirm("Do you want to leave the page")
+true
+confirm("Do you want to leave the page")
+false
+var a = confirm("Do you want to leave the page")
+undefined
+a
+true
+var a = confirm("Do you want to leave the page")
+undefined
+a
+false
+=> Return Boolean value on the basis of user input
+
+prompt("Please tell me your name")
+
+prompt("Please tell me your name")
+null
+prompt("Please tell me your name")
+"aakash"
+prompt("Please tell me your name")
+"john"
+var name = prompt("Please tell me your name")
+undefined
+name
+"aakash"
+
+
+var a = prompt('Please tell first number')
+var b = prompt('Please tell second number')
+console.log(a+b)
+
+var a = "1"
+var b = "1"
+a+b == "11"
+
+parseInt(a)+parseInt(b)
+
+var a = "1"
+var b = "1"
+undefined
+a+b
+"11"
+parseInt(a)+parseInt(b)
+2
+var c = "10.2243"
+undefined
+parseInt(a)+parseInt(b)+parseInt(c)
+12
+parseInt(a)+parseInt(b)+parseFloat(c)
+12.2243
+
+
+=> 
+var a = 10
+var b = 20
+a+b
+outout = "1020"
+you need take input from the user as number but output
+should conact as string
