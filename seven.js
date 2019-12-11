8875a645ae6cd07341d9f3577cdee4783ec1f689
aakashDeveloper <ahanda205@gmail.com>
2019-12-11 01:19:51 +0000

    a

---
+++ seven.js
@@ -0,0 +1,28 @@
+var a = 10
+
+if(a%2==0){
+    console.log("number is even")
+}else if(a%3==0){
+    console.log("Number is divisible by 3")
+}
+else{
+    console.log("number is odd")
+}
+
+
+// Ternery
+
+var a =10;
+
+a>12 ?"ok":"Not Ok"
+
+a>12?a+1:a-1
+
+if condition before question mark is true than 
+first part will execute else second 
+
+//Luck Game
+Generate One random number between 1 to 20
+Now take one input from the user as number
+if both number match than user win
+else user loose show that in alert 
---
+++ six.js
@@ -0,0 +1,39 @@
+var a = 11
+var a > 10
+var a = 11
+undefined
+a>11
+false
+a<11
+false
+a<10
+false
+a>=11
+true
+a<=11
+true
+
+var a = 10
+undefined
+a!==10
+false
+a!==11
+true
+a!=11
+true
+
+var a = true
+undefined
+!a
+false
+var b =!a
+undefined
+b
+false
+!b
+true
+
+
+true && true => true
+true && false => false
+true || false=> true
